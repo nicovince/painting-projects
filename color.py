8496f79f10ffe331510d87f95916dd8e1a7c2bc4
Nicolas VINCENT <nico.vince@gmail.com>
2024-10-17 13:51:23 +0200

    plot base and contrast paints

--- color.py
+++ color.py
@@ -9,27 +9,29 @@ import matplotlib as mpl
 import colorsys
 from colors import *
 
-fig = plt.figure()
 
-display_axes = fig.add_axes([0.1,0.1,0.8,0.8], projection='polar')
-display_axes._direction = 2*np.pi ## This is a nasty hack - using the hidden field to
-                                  ## multiply the values such that 1 become 2*pi
-                                  ## this field is supposed to take values 1 or -1 only!!
+def configure_axes(rowcolind):
+    axes = plt.subplot(rowcolind, projection='polar')
+    axes._direction = 2*np.pi ## This is a nasty hack - using the hidden field to
+                              ## multiply the values such that 1 become 2*pi
+                              ## this field is supposed to take values 1 or -1 only!!
+    axes.set_axis_off()
+    # Plot the colorbar onto the polar axis
+    # note - use orientation horizontal so that the gradient goes around
+    # the wheel rather than centre out
+    norm = mpl.colors.Normalize(0.0, 2*np.pi)
+    cb = mpl.colorbar.ColorbarBase(axes, cmap=colormaps.get_cmap('hsv'),
+                                   norm=norm, orientation='horizontal')
+    # aesthetics - get rid of border and axis labels
+    cb.outline.set_visible(False)
 
-norm = mpl.colors.Normalize(0.0, 2*np.pi)
+    return axes
 
-# Plot the colorbar onto the polar axis
-# note - use orientation horizontal so that the gradient goes around
-# the wheel rather than centre out
-quant_steps = 2056
-cb = mpl.colorbar.ColorbarBase(display_axes, cmap=colormaps.get_cmap('hsv'),
-                                   norm=norm,
-                                   orientation='horizontal')
+base_colors_axes = configure_axes(221)
+contrast_axes = configure_axes(223)
 
-# aesthetics - get rid of border and axis labels
-cb.outline.set_visible(False)
-display_axes.set_axis_off()
-
-add_colors(display_axes, colors)
-display_axes.legend()
+add_colors(base_colors_axes, colors)
+add_colors(contrast_axes, contrasts)
+base_colors_axes.legend(bbox_to_anchor=(1.05, 1), loc=2)
+contrast_axes.legend(bbox_to_anchor=(1.05, 1), loc=2)
 plt.show() # Replace with plt.savefig if you want to save a file
