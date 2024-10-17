66afb511752795efea424357333e5d6d5bdbf47c
Nicolas VINCENT <nico.vince@gmail.com>
2024-10-17 13:51:13 +0200

    Add contrast paints

--- colors.py
+++ colors.py
@@ -11,6 +11,20 @@ colors = {
     'Kantor Blue': 0x06234f,
 }
 
+contrasts = {
+    'Moody Mauve': 0xA65285,
+    'Highlord Blue': 0x335884,
+    'Space Wolves Grey': 0x92B2CB,
+    'Absolution Green': 0x243B1F,
+    'Plaguebearer Flesh': 0xCCD1A9,
+    'Aggaros Dunes': 0xD5BF73,
+    'Snakebite Leather': 0xB66E19,
+    'Fire Giant Orange': 0xC84916,
+    'Guilliman Flesh': 0xD5A79A,
+    'Wyldwood': 0x715551,
+    'Basilicanum Grey': 0x9D9D9D,
+}
+
 def hexcode_to_rgb(code):
     r = ((code >> 16) & 0xff) / 0xff
     g = ((code >> 8) & 0xff) / 0xff
